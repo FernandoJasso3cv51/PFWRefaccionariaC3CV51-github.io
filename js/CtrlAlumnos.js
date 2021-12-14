8c26289987350cedb25f93be08a2f2c03e1e0dfe
FernandoJasso3cv51 <Fernando>
2021-12-14 16:02:45 -0600

    .

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -16,7 +16,7 @@ const lista = document.
   querySelector("#lista");
 const daoAlumno =
   getFirestore().
-    collection("Alumno");
+    collection("Producto");
 
 getAuth().
   onAuthStateChanged(
